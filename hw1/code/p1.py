a26884d080c5721ca1bde8f41da28d211e90f28e
Taek Lim <tio.taek.lim@gmail.com>
2026-09-13 21:44:58 -0500

    init

---
+++ hw1/code/p1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+"""
+1. observation:
+2. variables:
+3. unique restaurants:
+4. food types:
+5. each variable data type:
+6. missing value counts:
+7. appropriate 
+    - serving size:
+    - calories:
+    - sodium:
+    - sugars:
+8. present my result clearly
+9. 2-3 sentences to describe what I learned?
+"""
+n = dp.shape[0]
+print(n)
